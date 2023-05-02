54b446c69b75cd12149da676a26c249be4736c35
VitorSMaia <vitor.smaia1@gmail.com>
2023-05-02 17:12:07 -0300

    Add [Google Tag Manager]

--- resources/views/layout/blog.blade.php
+++ resources/views/layout/blog.blade.php
@@ -9,8 +9,20 @@
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
     @livewireStyles
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-M286PLP');</script>
+    <!-- End Google Tag Manager -->
 </head>
-<body class="bg-[#0F172A] px-5 md:px-40  text-white font-mono ">
+<body class="bg-[#0F172A] px-5 md:px-40  text-white font-mono">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M286PLP"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 <div class="flex justify-between items-center pt-5">
     <a href="/">VitorSM Blog</a>
     <div class="hidden lg:flex gap-x-5">
@@ -35,5 +47,6 @@
     <p>© Copyright {{ now()->format('Y') }} by Vitor Maia.</p>
 </footer>
 @livewireScripts
+
 </body>
 </html>
