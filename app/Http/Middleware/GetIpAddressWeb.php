6adc1d159d8e09deb1db242a38f4fb9664dd5116
VitorSMaia <vitor.smaia1@gmail.com>
2023-05-04 21:28:10 -0300

    Ajuste Layout Blog

--- app/Http/Middleware/GetIpAddressWeb.php
+++ app/Http/Middleware/GetIpAddressWeb.php
@@ -17,7 +17,7 @@ class GetIpAddressWeb
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $dataLocation = \Stevebauman\Location\Facades\Location::get($request->ip);
+        $dataLocation = \Stevebauman\Location\Facades\Location::get($this->getIp());
 
         $data['ip'] = $dataLocation->ip;
         $data['countryName'] = $dataLocation->countryName;
@@ -32,5 +32,19 @@ class GetIpAddressWeb
             'ip' => $data['ip']
         ],$data);
         return $next($request);
+    }
+
+    public function getIp(){
+        foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
+            if (array_key_exists($key, $_SERVER) === true){
+                foreach (explode(',', $_SERVER[$key]) as $ip){
+                    $ip = trim($ip); // just to be safe
+                    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
+                        return $ip;
+                    }
+                }
+            }
+        }
+        return request()->ip(); // it will return the server IP if the client IP is not found using this method.
     }
 }
